bb3c262ae9e19d12a37efbdfa6c701afa46ce2f5
Yohran Kallahari <yohranv@gmail.com>
2023-10-02 20:53:46 -0300

    Estruturando projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 import Home from './components/pages/Home';
-// import Company from './components/pages/Company';
-// import Contact from './components/pages/Contact';
-// import NewProject from './components/pages/NewProject';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/pages/layout/container';
 
 
 
@@ -12,15 +13,21 @@ function App() {
 
   return (
         <BrowserRouter>
-
-
-            <Routes>
-              <Route path='/' element={<Home />} />
-              {/* <Route path='/company' element={<Company />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/newproject' element={<NewProject />}></Route> */}
-            </Routes>
-
+        <ul>
+          <Link to="/">Home |</Link>
+          <Link to="company">Company |</Link>
+          <Link to="contact">Contact |</Link>
+          <Link to="newproject">New Project |</Link>
+        </ul>
+          <Container customClass="min-height">
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route path='/company' element={<Company />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/newproject' element={<NewProject />}></Route>
+              </Routes>
+          </Container>
+            <div>Footer</div>
         </BrowserRouter>
   );
 }
---
+++ src/components/pages/layout/container.js
@@ -0,0 +1,17 @@
+import style from "./Container.module.css"
+
+
+
+
+export default function Container(props){
+
+
+
+    return(
+        <div className={`${style.container} ${style[props.customClass]}`}>
+
+            {props.children}
+
+        </div>
+    )
+}
