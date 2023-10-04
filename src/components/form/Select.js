493cb15edd27fb1a9b7482af3168dec5e52bfa40
Yohran Kallahari <yohranv@gmail.com>
2023-10-04 20:26:54 -0300

    Conectando com API pelo React

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -7,6 +7,9 @@ export default function Select({ text, name, options, handleOnChange, value}){
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
                 <option>Selecione uma Opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,22 +2,39 @@ import style from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
+import { useEffect, useState } from 'react'
 
 export default function ProjectForm({btnText}){
+    const [categories, setCategories] = useState([])
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'content-type': 'application/json',
+        },
+    }).then(res => res.json())
+    .then(data => {
+        setCategories(data)
+    })
+    .catch(error => console.log(error))
+    },[])
+
     return(
         <form className={style.form}>
             <Input 
             type="text" 
             text="Nome do Projeto" 
             name="name" 
-            placeholder="Insira o nome do projeto" />
+            placeholder="Insira o nome do projeto" 
+            />
             <Input 
             type="number" 
             text="Orçamento do Projeto" 
             name="orcamento" 
-            placeholder="Insira o orçamento total" />
+            placeholder="Insira o orçamento total" 
+            />
 
-            <Select name="category_id" text="Selecione a Categoria" />
+            <Select name="category_id" text="Selecione a Categoria" options={categories}/>
             
             <SubmitButton text={btnText} />
         </form>
