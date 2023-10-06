08fc61e005cc1fe83184906e98244bc7ef791a4e
Yohran Kallahari <yohranv@gmail.com>
2023-10-05 21:08:57 -0300

    Mensagens do sistema

---
+++ src/components/layout/Message.js
@@ -0,0 +1,29 @@
+import style from './Message.module.css'
+import React, {useState, useEffect} from 'react'
+
+export default function Message({type, msg}){
+
+    const [visible, setVisible] = useState(false)
+    
+    useEffect(()=>{
+        if(!msg){
+            setVisible(false)
+            return
+        }else{
+            setVisible(true)
+
+            const timer = setTimeout(()=>{
+                setVisible(false)
+            }, 3000)    
+            return ()=> clearTimeout(timer)
+        }
+    },[msg])
+
+    return(
+        <>
+            {visible && (
+                <div className={`${style.message} ${style[type]}`}>{msg}</div>
+            )}
+        </>
+    )
+}
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,14 +1,21 @@
 import { useLocation } from 'react-router-dom'
+import Message from '../layout/Message'
 
 export default function Projects(){
     const location = useLocation()
 
-    const message = location.state && location.state.message
+    // const message = location.state && location.state.message
+    let message = ''
+
+    if(location.state){
+        message = location.state.message
+    }
 
     return(
         <div>
-            <h1>Projects</h1>
-            {message && <div>{message}</div>}
+            <h1>Meus Projetos</h1>
+            
+            {message && <Message type="success" msg={message} />}
         </div>
         
     )
